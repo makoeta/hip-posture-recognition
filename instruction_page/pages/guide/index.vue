eed4de397290e3db8fae114d53b00d9c057e98a5
Maximilian König <makoeta@gmx.de>
2025-07-12 18:33:19 +0200

    update: font size

--- instruction_page/pages/guide/index.vue
+++ instruction_page/pages/guide/index.vue
@@ -16,8 +16,8 @@ const topics = ref<MenuItem[]>(procedure);
 
 <template>
   <div class="flex min-h-max flex-col">
-    <div class="text-5xl font-bold 2xl:text-8xl">Inhalt</div>
-    <div class="flex flex-col text-3xl 2xl:text-5xl">
+    <div class="text-5xl font-bold 2xl:text-7xl">Inhalt</div>
+    <div class="flex flex-col text-3xl 2xl:text-4xl">
       <div
         v-for="(topic, index) in topics"
         :key="topic.label"
@@ -31,7 +31,7 @@ const topics = ref<MenuItem[]>(procedure);
               {{ topic.label }}
             </div>
           </div>
-          <div class="collapse-content text-xl 2xl:text-3xl">
+          <div class="collapse-content text-xl 2xl:text-2xl">
             <div
               v-for="(subItem, subIndex) in topic.subItems"
               :key="subItem.label"
