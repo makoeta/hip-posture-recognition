4f541509d4fcd312b5e49558f963078fe3966231
Maximilian König <makoeta@gmx.de>
2025-07-12 18:33:29 +0200

    update: end card

--- instruction_page/components/GuideEndcard.vue
+++ instruction_page/components/GuideEndcard.vue
@@ -3,17 +3,25 @@
 <template>
   <div class="flex flex-col items-center justify-center text-2xl">
     <div class="mb-4">
-      <slot name="text" />
+      <slot name="text"/>
     </div>
 
     <div class="mt-12">
       <NuxtLink
-        external
-        target="_blank"
-        to="https://github.com/makoeta/hip-posture-recognition"
+          external
+          target="_blank"
+          to="https://github.com/makoeta/hip-posture-recognition"
       >
         <button class="btn btn-xl">
-          <slot name="btnLabel" />
+          <slot name="toProjectLabel"/>
+        </button>
+      </NuxtLink>
+    </div>
+
+    <div class="mt-6">
+      <NuxtLink target="_blank" to="/guide/faq">
+        <button class="btn btn-xl">
+          <slot name="toFaqLabel"/>
         </button>
       </NuxtLink>
     </div>
